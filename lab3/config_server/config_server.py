43c739e0d87631b24193f2b5283913f8665e73e2
TymofiiShao <113201232+TymofiiShao@users.noreply.github.com>
2025-05-14 22:46:35 +0300

    Add files via upload

---
+++ lab3/config_server/config_server.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# "Сервісний реєстр"
+SERVICES = {
+    "logging-service": [
+        "127.0.0.1:50051",
+        "127.0.0.1:50052",
+        "127.0.0.1:50053"
+    ],
+    "messages-service": [
+        "127.0.0.1:6000"  # Приклад, якщо буде реалізовано
+    ]
+}
+
+@app.route("/services/<service_name>", methods=["GET"])
+def get_service_instances(service_name):
+    if service_name in SERVICES:
+        return jsonify(SERVICES[service_name])
+    return jsonify([]), 404
+
+if __name__ == "__main__":
+    app.run(port=8000)
